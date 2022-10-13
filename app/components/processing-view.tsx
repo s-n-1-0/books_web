8e3016c2b7609228905273c5d33d39e3e5b3c75b
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-10-13 17:58:57 +0900

    add ProcessingView component

---
+++ app/components/processing-view.tsx
@@ -0,0 +1,11 @@
+function ProcessingView() {
+  return (
+    <div className="flex justify-center p-10">
+      <div className="animate-ping h-2 w-2 bg-my-color rounded-full"></div>
+      <div className="animate-ping h-2 w-2 bg-my-color rounded-full mx-4"></div>
+      <div className="animate-ping h-2 w-2 bg-my-color rounded-full"></div>
+    </div>
+  );
+}
+
+export default ProcessingView;
