d988279905579d7ca4b89fea2071ad95058d415f
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-10-14 00:17:45 +0900

    isbnを指定しなかった場合の検索ボックスを追加

--- app/pages/ja/share.tsx
+++ app/pages/ja/share.tsx
@@ -7,18 +7,59 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useState } from "react";
-
 const Home: NextPage = () => {
   const router = useRouter();
   const { isbn } = router.query;
   const [bookData, setBookData] = useState<OpenBDBookData | null>(null);
+  const [editingIsbn, setEdittingIsbn] = useState<string>("");
+  let isHello = false;
   if (typeof isbn == "string") {
     openbd.get(isbn).then((res: { data: OpenBDGetResponseData }) => {
       setBookData(res.data?.[0]);
     });
+  } else {
+    //isbn未指定の場合
+    isHello = true;
   }
 
   function makeMainContent() {
+    if (isHello) {
+      return (
+        <div className="text-center">
+          <h1 className="pt-5 text-3xl pb-2">書籍情報を共有</h1>
+          <div className="mb-4 flex items-end">
+            <div className="w-full">
+              <label className="block text-gray-700 text-sm font-bold mb-2 text-left">
+                ISBN
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="email"
+                placeholder="9784798056920"
+                value={editingIsbn}
+                onChange={(e) => {
+                  setEdittingIsbn(e.target.value);
+                }}
+              />
+            </div>
+            {(() => {
+              if (editingIsbn != "")
+                return (
+                  <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ml-2 w-100 whitespace-nowrap"
+                    onClick={() => {
+                      location.href = "./share?isbn=" + editingIsbn;
+                    }}
+                  >
+                    調べる
+                  </button>
+                );
+              else return;
+            })()}
+          </div>
+        </div>
+      );
+    }
     if (!bookData) {
       return (
         <ul>
