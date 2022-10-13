7e30ef2c4b7854ba9b66fddc4c6cf60884f55fe4
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-10-14 00:22:28 +0900

    update text

--- app/pages/ja/share.tsx
+++ app/pages/ja/share.tsx
@@ -29,7 +29,7 @@ const Home: NextPage = () => {
           <div className="mb-4 flex items-end">
             <div className="w-full">
               <label className="block text-gray-700 text-sm font-bold mb-2 text-left">
-                ISBN
+                ISBNで調べる
               </label>
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
