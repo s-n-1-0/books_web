1ef566f8015144c315d76ee9091f1dd61cec8ab0
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-10-23 16:05:17 +0900

    紀伊國屋とHontoのリンクを追加

--- app/components/books/book-links.tsx
+++ app/components/books/book-links.tsx
@@ -1,23 +1,21 @@
 import { faAmazon } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { convertIsbn2Url } from "asin2isbn";
+import { convertIsbn, convertIsbn2Url } from "asin2isbn";
 import TwButton from "../tw-button";
 
 type Props = {
   isbn: string;
 };
 function BookLinks({ isbn }: Props) {
+  let amazonUrl = convertIsbn2Url(isbn);
+  let isbn13 = isbn.length == 13 ? isbn : convertIsbn(isbn);
   return (
-    <div className="flex justify-center m-3">
-      <a
-        href={(() => {
-          return convertIsbn2Url(isbn);
-        })()}
-      >
+    <div className="flex justify-center items-center m-3">
+      <a href={amazonUrl} target="_blank" rel="noopener noreferrer">
         <TwButton
           color={{
-            color: "bg-neutral-500",
-            hoverColor: "hover:bg-neutral-700",
+            color: "bg-neutral-700",
+            hoverColor: "hover:bg-neutral-900",
           }}
         >
           <span>
@@ -25,6 +23,38 @@ function BookLinks({ isbn }: Props) {
             <span className="ml-2">Amazon</span>
           </span>
         </TwButton>
+      </a>
+      <a
+        href={"https://www.kinokuniya.co.jp/f/dsg-01-" + isbn13}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="m-2"
+      >
+        <img
+          src="https://corp.kinokuniya.co.jp/wp-content/uploads/2020/07/logo.png"
+          alt="紀伊國屋ロゴ"
+          style={{
+            height: "125px",
+            width: "inherit",
+            objectFit: "contain",
+          }}
+        />
+      </a>
+      <a
+        href={"https://honto.jp/netstore/search.html?isbn=" + isbn13}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="m-2"
+      >
+        <img
+          src="https://i.gyazo.com/ee86b2469f712858a4e1b0ce17684939.png"
+          alt="紀伊國屋ロゴ"
+          style={{
+            height: "50px",
+            width: "inherit",
+            objectFit: "contain",
+          }}
+        />
       </a>
     </div>
   );
