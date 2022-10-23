403e8eeea4e41bb20073d4eb7143c87944f72962
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-10-24 00:09:17 +0900

    TweetButtonコンポーネントを動的データ対応

--- app/components/tweet-button.tsx
+++ app/components/tweet-button.tsx
@@ -1,25 +1,38 @@
-import { useEffect } from "react";
-let isLoad = false;
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 type Props = {
   text: string;
+  url?: string;
 };
-function TweetButton({ text }: Props) {
-  useEffect(() => {
-    if (isLoad) return;
-    const script = document.createElement("script");
-    script.src = "https://platform.twitter.com/widgets.js";
-    document.body.appendChild(script);
-    isLoad = true;
-  }, []);
+function TweetButton({ text, url = encodeURIComponent(location.href) }: Props) {
+  let buttonStyle = css`
+    text-align: end;
+    border: none;
+    background-color: #1da1f2;
+    padding: 6px 16px;
+    border-radius: 100vh;
+    color: white;
+    cursor: pointer;
+  `;
+
   return (
-    <a
-      href="https://twitter.com/share?ref_src=twsrc%5Etfw"
-      className="twitter-share-button"
-      data-text={text + " - Share Books"}
-      data-show-count="false"
+    <button
+      css={buttonStyle}
+      onClick={() => {
+        window.open(
+          "https://twitter.com/share?text=" +
+            text +
+            " - Share Books" +
+            "&url=" +
+            encodeURIComponent(url)
+        );
+      }}
     >
-      Tweet
-    </a>
+      <FontAwesomeIcon icon={faTwitter} />
+      ツイートする
+    </button>
   );
 }
 
