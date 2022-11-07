6229169d7b0c1661509c3f31fea8caecfee7dfd0
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-11-08 00:14:42 +0900

    トップページを仮追加

---
+++ app/pages/index.tsx
@@ -0,0 +1,38 @@
+import CustomHead from "@/components/head";
+import Header from "@/components/header";
+import { NextPage } from "next";
+import Link from "next/link";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <CustomHead
+        title="Share Books"
+        pageUrl="https://books.sn-10.net"
+        ogType="website"
+      ></CustomHead>
+      <Header isMenu={false}></Header>
+      <main>
+        <div className="w-full px-2 py-3 text-center">
+          <div className="py-20">
+            <Link href="/ja/share">
+              <a className="inline-block text-xl px-4 py-2 leading-none border rounded border-black hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">
+                本を探す
+              </a>
+            </Link>
+          </div>
+        </div>
+      </main>
+
+      <footer>
+        <hr />
+        <p className="text-center">
+          <a className="underline" href="https://hello.sn-10.net">
+            sn-10.net
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
+};
+export default Home;
