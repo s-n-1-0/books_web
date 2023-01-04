de42ae35760e4a10f307f2bb9519d6f6484276b2
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-01-04 19:59:25 +0900

    SearchGoogleBooksModalの検索表示部分を分離(->./list.tsx)

--- app/components/books/search-googlebooks/modal.tsx
+++ app/components/books/search-googlebooks/modal.tsx
@@ -1,34 +1,19 @@
-import {
-  GoogleBooksApiBookData,
-  GoogleBooksApiVolumesResponseData,
-} from "@/Interfaces/googlebooks/volumes";
-import { searchGoogleBooksApi } from "@/libs/googlebooks";
-import { makeSharePageLink } from "@/utils/links";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { forwardRef, Ref, useImperativeHandle, useState } from "react";
-import ProcessingView from "../../processing-view";
-import BookThumbnail from "../book-thumbnail";
+import { forwardRef, Ref, useImperativeHandle, useRef, useState } from "react";
+import { SearchGoogleBooksList, SearchGoogleBooksListRefType } from "./list";
 function _SearchGoogleBooksModal(_: any, ref: Ref<unknown>) {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [searchTitle, setSearchTitle] = useState<string>("");
-  const [googleBooksResults, setGoogleBooksResults] = useState<
-    GoogleBooksApiBookData[]
-  >([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const googlebooksListRef = useRef<SearchGoogleBooksListRefType>(null);
   async function openModal(title: string) {
-    setIsLoading(true);
     setIsOpen(true);
-    let res = await searchGoogleBooksApi({
-      q: {
-        intitle: title,
-      },
-    });
     setSearchTitle(title);
-    setGoogleBooksResults(
-      (res.data as GoogleBooksApiVolumesResponseData)?.items ?? []
-    );
-    setIsLoading(false);
+    process.nextTick(() => {
+      console.log(googlebooksListRef.current);
+      googlebooksListRef.current?.search(title);
+    });
   }
   function closeModal() {
     setIsOpen(false);
@@ -52,54 +37,7 @@ function _SearchGoogleBooksModal(_: any, ref: Ref<unknown>) {
           />
         </div>
         <hr className="mb-3" />
-        {(() => {
-          if (isLoading)
-            return (
-              <div className="text-center">
-                <ProcessingView />
-              </div>
-            );
-          return (
-            <ul className="m-2 text-left text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white cursor-pointer">
-              {googleBooksResults
-                .filter((item) => {
-                  let id = item?.volumeInfo?.industryIdentifiers?.[0];
-                  if (!id) return false;
-                  return id.type == "ISBN_10" || id.type == "ISBN_13";
-                })
-                .map((item) => {
-                  return (
-                    <li
-                      key={item.id}
-                      onClick={() => {
-                        let url = makeSharePageLink(
-                          item.volumeInfo.industryIdentifiers[0].identifier,
-                          "googlebooks",
-                          ""
-                        );
-                        window.open(url, "_blank");
-                      }}
-                      className="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600"
-                    >
-                      <div className="flex items-center">
-                        <BookThumbnail
-                          src={item.volumeInfo.imageLinks?.smallThumbnail ?? ""}
-                        />
-                        <div className="pl-2">
-                          {item.volumeInfo.title}
-                          <br />
-                          <small className="text-secondary">
-                            ISBN :{" "}
-                            {item.volumeInfo.industryIdentifiers[0].identifier}
-                          </small>
-                        </div>
-                      </div>
-                    </li>
-                  );
-                })}
-            </ul>
-          );
-        })()}
+        <SearchGoogleBooksList ref={googlebooksListRef} />
       </div>
     </div>
   );
