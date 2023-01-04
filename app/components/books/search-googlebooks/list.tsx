a66575c572ca1b65a4d6a999c1fd2489391e2f82
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-01-04 20:24:21 +0900

    GoogleBooksタイトル検索でヒットがない場合そのことを通知するように修正

--- app/components/books/search-googlebooks/list.tsx
+++ app/components/books/search-googlebooks/list.tsx
@@ -36,43 +36,54 @@ function _SearchGoogleBooksList(_: any, ref: Ref<unknown>) {
       </div>
     );
   return (
-    <ul className="m-2 text-left text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white cursor-pointer">
-      {googleBooksResults
-        .filter((item) => {
-          let id = item?.volumeInfo?.industryIdentifiers?.[0];
-          if (!id) return false;
-          return id.type == "ISBN_10" || id.type == "ISBN_13";
-        })
-        .map((item) => {
-          return (
-            <li
-              key={item.id}
-              onClick={() => {
-                let url = makeSharePageLink(
-                  item.volumeInfo.industryIdentifiers[0].identifier,
-                  "googlebooks",
-                  ""
-                );
-                window.open(url, "_blank");
-              }}
-              className="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600"
-            >
-              <div className="flex items-center">
-                <BookThumbnail
-                  src={item.volumeInfo.imageLinks?.smallThumbnail ?? ""}
-                />
-                <div className="pl-2">
-                  {item.volumeInfo.title}
-                  <br />
-                  <small className="text-secondary">
-                    ISBN : {item.volumeInfo.industryIdentifiers[0].identifier}
-                  </small>
+    <div>
+      {" "}
+      <ul className="m-2 text-left text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white cursor-pointer">
+        {googleBooksResults
+          .filter((item) => {
+            let id = item?.volumeInfo?.industryIdentifiers?.[0];
+            if (!id) return false;
+            return id.type == "ISBN_10" || id.type == "ISBN_13";
+          })
+          .map((item) => {
+            return (
+              <li
+                key={item.id}
+                onClick={() => {
+                  let url = makeSharePageLink(
+                    item.volumeInfo.industryIdentifiers[0].identifier,
+                    "googlebooks",
+                    ""
+                  );
+                  window.open(url, "_blank");
+                }}
+                className="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600"
+              >
+                <div className="flex items-center">
+                  <BookThumbnail
+                    src={item.volumeInfo.imageLinks?.smallThumbnail ?? ""}
+                  />
+                  <div className="pl-2">
+                    {item.volumeInfo.title}
+                    <br />
+                    <small className="text-secondary">
+                      ISBN : {item.volumeInfo.industryIdentifiers[0].identifier}
+                    </small>
+                  </div>
                 </div>
-              </div>
-            </li>
+              </li>
+            );
+          })}
+      </ul>
+      {(() => {
+        if (googleBooksResults.length == 0)
+          return (
+            <p className="text-center text-secondary">
+              該当する書籍がヒットしませんでした...
+            </p>
           );
-        })}
-    </ul>
+      })()}
+    </div>
   );
 }
 export let SearchGoogleBooksList = forwardRef(_SearchGoogleBooksList);
