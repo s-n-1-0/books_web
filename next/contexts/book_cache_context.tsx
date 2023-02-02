4db7537e039d4b235966cac6a6f46dda72781a26
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-02-02 16:03:54 +0900

    書籍情報をキャッシュするProviderを追加

---
+++ next/contexts/book_cache_context.tsx
@@ -0,0 +1,38 @@
+import { BookData } from "@/utils/links";
+import { createContext, ReactNode, useContext, useState } from "react";
+
+export interface BookCacheContextType {
+  bookDataCaches: { [isbn: string]: BookData };
+  addBookData: (isbn: string, bookData: BookData) => void;
+}
+
+export const BookCacheContext = createContext<BookCacheContextType>({
+  bookDataCaches: {},
+  addBookData: (isbn: string, bookData: BookData) => {},
+});
+
+export const BookCacheContextProvider = ({
+  children,
+}: {
+  children?: ReactNode;
+}) => {
+  const context: BookCacheContextType = useContext(BookCacheContext);
+  const [bookDataCaches, setBookDataCaches] = useState(context.bookDataCaches);
+
+  const newContext: BookCacheContextType = {
+    bookDataCaches: bookDataCaches,
+    addBookData(isbn, bookData) {
+      if (!(isbn in bookDataCaches)) {
+        let newCaches = { ...bookDataCaches };
+        newCaches[isbn] = bookData;
+        setBookDataCaches(newCaches);
+      }
+    },
+  };
+
+  return (
+    <BookCacheContext.Provider value={newContext}>
+      {children}
+    </BookCacheContext.Provider>
+  );
+};
